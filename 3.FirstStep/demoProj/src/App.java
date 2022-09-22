a0e48aa8611c81355c976b520bb876db380bc0d6
harikrishnan ak <harikrishnan.ak@giglabz.com>
2022-09-22 12:10:52 +0530

    new java proj for DS ANd algo

--- 3.FirstStep/demoProj/src/App.java
+++ 3.FirstStep/demoProj/src/App.java
@@ -1,5 +1,7 @@
 package demoProj.src;
 
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Helloooooooo, World!");
@@ -9,6 +11,21 @@ public class App {
         double myDouble = 5.67d;
         String outString = String.format("int:%2d ,float: %f , double : %f", myInt,myFloat,myDouble);
         System.out.println(outString);
+
+        // pounds to kg
+        System.out.println("enter value in pound");
+        Scanner in = new Scanner(System.in);
+        double pounds= in.nextDouble();
+        double kg= pounds * 0.45359237;
+        System.out.println("value in Kg ="+kg);
+
+        // For more precision scenarios we use a class - BigDecimal.
+
+        //  char type;
+        char mychar = 'A';
+        char mycharWithUniCode  = '\u0041';
+        char copyRight ='\u00A9';
+        System.out.println("mychar= "+mychar+" , mycharWithUnicode= "+mycharWithUniCode+"copyright = "+copyRight);
 
     }
 }
